a5cad16731b22c5bad204a4a67f385b3c1dfafa4
Thiago <thiagosouf@gmail.com>
2022-02-15 13:42:31 -0300

    tela 1 mostrando os quizzes

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let cardQuizz
+
+function buscarQuizz() {
+    setInterval(displayQuizz, 3000);
+        function displayQuizz() {
+            const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+            promessa.then(promessaCumprida);
+  }}
+
+  function promessaCumprida(resposta) {
+    quantidadeDeCards = resposta.data.length;
+    console.log("quantidade de cards"+quantidadeDeCards);
+    cardQuizz = resposta.data;
+    renderizarQuizzes(cardQuizz);
+    }
+
+function renderizarQuizzes(cardQuizz){
+    let divQuizzes = document.querySelector(".quizzes")
+    divQuizzes.innerHTML = ""
+    for (let i = 0; i < quantidadeDeCards; i++ ){
+        divQuizzes.innerHTML += `
+        <div class="container card-quizz">
+                    <img src="${cardQuizz[i].image}" alt="">
+                    <span>${cardQuizz[i].title}</span>
+                </div>
+        `
+    }
+}
